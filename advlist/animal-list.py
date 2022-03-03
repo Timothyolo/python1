33d8e119395d930f3447ad756e70797686d6b9ca
Timothyolo <timothyolo@users.noreply.github.com>
2022-03-03 15:11:53 +0000

    About complex lists

---
+++ advlist/animal-list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""Alta3 Research | RZFeeser
+   List - simple list"""
+
+def main():
+    # create a list called animals
+    animals = ["Hen", "Fox", "Bee", "Koi", "Cow","Dog", "Cat"]
+
+    # display animals
+    print(animals)
+
+main()
+
