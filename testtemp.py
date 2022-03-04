1f7d816e1e3ba097e8ee54dc060388307df27ca0
Timothyolo <timothyolo@users.noreply.github.com>
2022-03-04 16:16:14 +0000

    learning loops

---
+++ testtemp.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+temp = 25
+input=""
+if (temp<10):
+    print("cold" "+" input) 
+elif(temp>25):
+    print("hot")
+elif(temp<25):
+    print("average")
+else:
+    print("perfect")
---
+++ whiletest.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+
+i=1
+while i<=10:
+    print("learning in progress")
+    i+=1
